e5d9126a5b0d2555ccaf4dad9e1a7467cede843a
ASHIS <aasiiishh@gmail.com>
2026-01-26 12:39:16 +0530

    Update calendar.js with January calendar

--- js/calendar.js
+++ js/calendar.js
@@ -18,7 +18,11 @@ auth.onAuthStateChanged(async user => {
   renderCalendar();
 });
 
-/* LOAD TASKS */
+/* LOAD TASKS 1	2	3
+4	5	6	7	8	9	10
+11	12	13	14	15	16	17
+18	19	20	21	22	23	24
+25	26	27	28	29	30	31 */
 async function loadTasks() {
   const snap = await getDocs(
     collection(db, "users", uid, "tasks")
